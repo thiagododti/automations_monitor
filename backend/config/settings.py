0378d5b4f5071f350553d9985cae0db898500454
Thiago de Oliveira Dantas <t.dod@live.com>
2026-03-20 15:14:12 -0300

    a

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,8 +6,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env_path = BASE_DIR.parent / "stack.env"
-load_dotenv(dotenv_path=env_path)
+load_dotenv(dotenv_path='stack.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
