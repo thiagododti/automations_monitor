a735daac148cba76e0fcaada3aeb572fd7c32b4f
Thiago de Oliveira Dantas <t.dod@live.com>
2026-03-25 14:57:33 -0300

    Update jobs.py

--- backend/apps/scheduler/jobs.py
+++ backend/apps/scheduler/jobs.py
@@ -14,15 +14,18 @@ def check_stuck_executions():
         date_end__isnull=True,
         last_update__lt=timeout,
     )
+    print(f"{stuck_executions.count()} execuções travadas encontradas.")
 
     execution_ids = stuck_executions.values_list("id", flat=True)
 
-    Step.objects.filter(
+    steps_stucks = Step.objects.filter(
         execution_id__in=execution_ids,
         date_end__isnull=True,
     ).update(status=ExecutionStatus.ERRO)
+    if steps_stucks > 0:
+        print(f"{steps_stucks} steps travados foram atualizados para erro.")
 
-    stuck_executions.update(
+    exec_stuck = stuck_executions.update(
         status=ExecutionStatus.ERRO,
         date_end=timezone.now()
     )
